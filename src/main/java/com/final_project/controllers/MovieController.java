76f69ed48870ea48d9c957fe38d6cdefba70c14c
Patrick Nymark <pn@ctdenmark.dk>
2019-05-14 17:47:38 +0200

    fixed all movies route

--- src/main/java/com/final_project/controllers/MovieController.java
+++ src/main/java/com/final_project/controllers/MovieController.java
@@ -1,13 +1,28 @@
 package com.final_project.controllers;
 
+import com.final_project.entities.Movie;
+import com.final_project.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class MovieController {
 
-    @GetMapping ("/all-movie")
-    public String getAllMovie(){return "all-movie";}
+    @Autowired
+    MovieRepository movieRepository;
+
+    @GetMapping ("/all-movies")
+    public String getAllMovie(Model model){
+        List<Movie> movieList = movieRepository.getAllMovies();
+
+        model.addAttribute("movies", movieList);
+
+        return "all-movies";
+    }
 
 
 

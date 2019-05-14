82f6f2c637267e8dd3786eb66dc8925030cc43fb
Patrick Nymark <pn@ctdenmark.dk>
2019-05-15 00:19:09 +0200

    removed import

--- src/main/java/com/final_project/controllers/MoviePlayController.java
+++ src/main/java/com/final_project/controllers/MoviePlayController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;

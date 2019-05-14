0356d9dbc2427c4717d3aed0a366a1f0bb12c282
Osi4lucy <soloosilamah@yahoo.com>
2019-05-14 12:40:35 +0200

    Added theater controller

--- src/main/java/com/final_project/controllers/TheaterController.java
+++ src/main/java/com/final_project/controllers/TheaterController.java
@@ -1,12 +1,40 @@
 package com.final_project.controllers;
 
+import com.final_project.entities.Theater;
+import com.final_project.repositories.TheaterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TheaterController {
+
+    @Autowired
+    TheaterRepository theaterRepository;
+
     @GetMapping ("/all-theater")
     public String getAllTheaters(){
         return "all-theater";
+    }
+    @GetMapping ("/add-theater")
+    public String getAddTheater() {
+        return "add-theater";
+    }
+
+    @PostMapping("/add-theater")
+    public String addTheater(@RequestParam("theater") String theater,
+                             @RequestParam("numberOfRows") int numbeOfRows,
+                             @RequestParam("seatsPerRow")int seatsPerRow
+
+    ) {
+        Theater newTheater = new Theater();
+        newTheater.setTheaterName(theater);
+        newTheater.setRowNumber(numbeOfRows);
+        newTheater.setSeatPerRow(seatsPerRow);
+        System.out.println();
+        theaterRepository.addTheater(newTheater);
+        return "redirect:/all-theater";
     }
 }

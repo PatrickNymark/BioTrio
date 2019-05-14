a23d8a1d2414e722520c152d192a548bdd351345
Patrick Nymark <pn@ctdenmark.dk>
2019-05-14 17:09:36 +0200

    added post route for add booking

--- src/main/java/com/final_project/controllers/BookingController.java
+++ src/main/java/com/final_project/controllers/BookingController.java
@@ -1,5 +1,6 @@
 package com.final_project.controllers;
 
+import com.final_project.entities.Booking;
 import com.final_project.entities.Seat;
 import com.final_project.repositories.BookingRepository;
 import com.final_project.repositories.SeatRepository;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -28,5 +31,20 @@ public class BookingController {
         return "choose-seat";
     }
 
+    @PostMapping("/booking/choose-seat/{moviePlayId}")
+    public String addBooking(
+            @PathVariable(name = "moviePlayId") int moviePlayId,
+            @RequestParam("seatRow") int seatRow,
+            @RequestParam("seatNr") int seatNr) {
 
+        Booking booking = new Booking();
+
+        booking.setMoviePlayId(moviePlayId);
+        booking.setSeatNr(seatNr);
+        booking.setSeatRow(seatRow);
+
+        bookingRepository.addBooking(booking);
+
+        return "redirect:/";
+    }
 }

53ac706489a31097558d20ab07f1f4a32548d037
Osi4lucy <soloosilamah@yahoo.com>
2019-05-14 12:41:29 +0200

    Added theater repository

--- src/main/java/com/final_project/repositories/TheaterRepository.java
+++ src/main/java/com/final_project/repositories/TheaterRepository.java
@@ -1,4 +1,19 @@
 package com.final_project.repositories;
 
+import com.final_project.entities.Theater;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.xml.Jdbc4SqlXmlHandler;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class TheaterRepository {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public int addTheater(Theater theater) {
+
+        return jdbcTemplate.update("INSERT INTO theaters(title, seats_pr_row, total_rows) VALUES (?,?,?)", theater.getTheaterName(), theater.getSeatPerRow(), theater.getRowNumber());
+    }
+
 }

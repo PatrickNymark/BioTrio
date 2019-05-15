ae471f0ecb66163f7b44e65ff124f4488170df84
Shehab <shehab_syed@hotmail.com>
2019-05-15 13:02:02 +0200

    created get theater by id route

--- src/main/java/com/final_project/controllers/TheaterController.java
+++ src/main/java/com/final_project/controllers/TheaterController.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -45,5 +45,12 @@ public class TheaterController {
 
         theaterRepository.addTheater(newTheater);
         return "redirect:/all-theater";
+    }
+    @GetMapping ("/theater/{id}")
+    public String getTheaterById (@PathVariable (name = "id")int id, Model model){
+
+        Theater theater = theaterRepository.findTheaterById(id);
+        model.addAttribute("theater", theater);
+        return "theater-page";
     }
 }
